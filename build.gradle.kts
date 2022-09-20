c06eb37e09696d68c762a72996ed477ad578a10e
Dejan Igrec <dejan@workerbase.com>
2022-09-20 21:30:27 +0200

    feat(build): update Gradle to v7.4 and AGP to v7.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,8 +20,8 @@ buildscript {
 
 plugins {
     // Android
-    id("com.android.library") version "7.2.2" apply false
-    id("com.android.application") version "7.2.2" apply false
+    id("com.android.library") version "7.3.0" apply false
+    id("com.android.application") version "7.3.0" apply false
 
     // Kotlin
     id("org.jetbrains.kotlin.android") version "1.7.10" apply false
