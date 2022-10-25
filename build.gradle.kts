43f19ba35d1ee74bf4baa51a84fa6fff5e8b1a22
Dejan Igrec <dejan@workerbase.com>
2022-10-25 22:30:42 +0200

    fix: update Kotlin to v1.7.20

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,7 @@ plugins {
     id("com.android.application") version "7.3.1" apply false
 
     // Kotlin
-    id("org.jetbrains.kotlin.android") version "1.7.10" apply false
+    id("org.jetbrains.kotlin.android") version "1.7.20" apply false
 }
 
 tasks.register("clean", Delete::class) {
