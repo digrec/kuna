b49d1aebb50e5f5c3f76c49e0a089e451f667991
Dejan Igrec <dejan.igrec@gmail.com>
2022-11-05 14:09:39 +0100

    feat: update Activity to v1.6.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
     val vTargetSdk by extra(33)
 
     // common dependency versions
-    val vActivity by extra("1.5.1")
+    val vActivity by extra("1.6.1")
     val vAndroidX by extra("1.9.0")
     val vComposeBom by extra("2022.10.00")
     val vComposeCompiler by extra("1.3.2")
