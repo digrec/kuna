df4fa1754ffeda0f5976fe446da1c2ff501c9616
Dejan Igrec <dejan.igrec@gmail.com>
2023-01-21 11:52:25 +0100

    feat: update Compose to v2023.01 and Compose Compiler to v1.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,8 +7,8 @@ buildscript {
     // common dependency versions
     val vActivity by extra("1.6.1")
     val vAndroidX by extra("1.9.0")
-    val vComposeBom by extra("2022.12.00")
-    val vComposeCompiler by extra("1.3.2")
+    val vComposeBom by extra("2023.01.00")
+    val vComposeCompiler by extra("1.4.0")
     val vEspresso by extra("3.5.0")
     val vJunit by extra("4.13.2")
     val vJunitExt by extra("1.1.4")
