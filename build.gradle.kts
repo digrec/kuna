1fae46c9e906b51bdd985e3e270f519e04621a63
Dejan Igrec <dejan.igrec@gmail.com>
2023-01-21 11:48:28 +0100

    feat: update Kotlin to v1.8

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,7 @@ plugins {
     id("com.android.application") version "7.4.0" apply false
 
     // Kotlin
-    id("org.jetbrains.kotlin.android") version "1.7.20" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.0" apply false
 }
 
 tasks.register("clean", Delete::class) {
