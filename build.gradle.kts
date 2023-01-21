8f900105373863e174a60263f7ceb92013188d35
Dejan Igrec <dejan.igrec@gmail.com>
2023-01-21 11:47:08 +0100

    feat: update Gradle to v7.5 and AGP to v7.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,8 +20,8 @@ buildscript {
 
 plugins {
     // Android
-    id("com.android.library") version "7.3.1" apply false
-    id("com.android.application") version "7.3.1" apply false
+    id("com.android.library") version "7.4.0" apply false
+    id("com.android.application") version "7.4.0" apply false
 
     // Kotlin
     id("org.jetbrains.kotlin.android") version "1.7.20" apply false
