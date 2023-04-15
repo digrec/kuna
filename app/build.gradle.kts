08e6444387c233f744cb02c559a0376cd0c73764
Dejan Igrec <dejan.igrec@gmail.com>
2023-04-15 21:22:39 +0200

    build: sign the release build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,6 +35,7 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
+            signingConfig = signingConfigs.getByName("debug")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
