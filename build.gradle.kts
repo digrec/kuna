0c9de158b325ccceae78134f2653a7a3a2ef927b
Dejan Igrec <dejan.igrec@gmail.com>
2023-08-28 22:03:05 +0200

    chore(deps): update Kotlin to v1.9

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ plugins {
     id("com.android.application") version "8.1.1" apply false
 
     // Kotlin
-    id("org.jetbrains.kotlin.android") version "1.8.21" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
 }
 
 tasks.register("clean", Delete::class) {
