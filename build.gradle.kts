80e1cdf4241d9a37dde35619210ac43d75c4e7d0
Dejan Igrec <dejan.igrec@gmail.com>
2023-09-29 12:21:48 +0200

    chore(deps): update AGP to v8.1.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,8 +26,8 @@ buildscript {
 
 plugins {
     // Android
-    id("com.android.library") version "8.1.1" apply false
-    id("com.android.application") version "8.1.1" apply false
+    id("com.android.library") version "8.1.2" apply false
+    id("com.android.application") version "8.1.2" apply false
 
     // Kotlin
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
