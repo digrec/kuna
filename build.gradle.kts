25f202b40c3f2b4a659bc01d85bd6dbf09c23f0d
Dejan Igrec <dejan.igrec@gmail.com>
2023-12-11 22:36:05 +0100

    chore(deps): update KotlinSerialization to v1.6.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ buildscript {
     val vJunitExt by extra("1.1.5")
     val vKoin by extra("3.5.0")
     val vKoinCompose by extra("3.5.0")
-    val vKotlinSerialization by extra("1.6.1")
+    val vKotlinSerialization by extra("1.6.2")
     val vKotlinxDateTime by extra("0.4.1")
     val vKtor by extra("2.3.6")
     val vLifecycle by extra("2.6.2")
