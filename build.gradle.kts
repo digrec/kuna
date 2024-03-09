854837372f68b567331bdcb9518f6836a303adc5
Dejan Igrec <dejan.igrec@gmail.com>
2024-03-09 21:37:46 +0100

    chore(deps): update Lifecycle to v2.7 Navigation to v2.7.7

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,8 +19,8 @@ buildscript {
     val vKotlinSerialization by extra("1.6.3")
     val vKotlinxDateTime by extra("0.5.0")
     val vKtor by extra("2.3.9")
-    val vLifecycle by extra("2.6.2")
-    val vNavigation by extra("2.7.6")
+    val vLifecycle by extra("2.7.0")
+    val vNavigation by extra("2.7.7")
     val vSlf4j by extra("2.0.10")
     val vTimber by extra("5.0.1")
 }
