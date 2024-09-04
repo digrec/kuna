9f550f0202213fc041a72e6bd311cfb68d76ea04
Dejan Igrec <dejan.igrec@gmail.com>
2024-09-04 21:36:05 +0200

    chore(deps): update Kotlin to v2.0.20

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,10 +31,10 @@ plugins {
     id("com.android.application") version "8.6.0" apply false
 
     // Kotlin
-    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.0" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.0.0" apply false
-    id("com.google.devtools.ksp") version "2.0.0-1.0.23" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.20" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.20" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.20" apply false
+    id("com.google.devtools.ksp") version "2.0.20-1.0.24" apply false
 }
 
 tasks.register("clean", Delete::class) {
